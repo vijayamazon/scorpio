37268ec842a63fbf3fe1d5a512c0be18a0c030d5
jibo <jibo@outlook.com>
2018-12-14 23:18:10 +0800

    1.1

--- src/main/java/com/onlymaker/scorpio/Welcome.java
+++ src/main/java/com/onlymaker/scorpio/Welcome.java
@@ -1,5 +1,7 @@
 package com.onlymaker.scorpio;
 
+import com.onlymaker.scorpio.mws.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -9,9 +11,12 @@ import org.springframework.stereotype.Component;
 public class Welcome implements ApplicationRunner {
     @Value("${mws.mode}")
     String mode;
+    @Autowired
+    Configuration configuration;
 
     @Override
     public void run(ApplicationArguments args) {
-        System.out.println("System startup in mode: " + mode);
+        System.out.println("========== WELCOME ==========");
+        System.out.println(String.format("%s %s startup in mode %s", configuration.getAppName(), configuration.getAppVersion(), mode));
     }
 }
