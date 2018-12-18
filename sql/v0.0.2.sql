66c9c2feec3d97239500418a74d641561b754a25
jibo <jibo@outlook.com>
2018-12-18 17:48:54 +0800

    Update stats view

--- sql/v0.0.2.sql
+++ sql/v0.0.2.sql
@@ -2,8 +2,10 @@ drop view if exists vo_store;
 create view vo_store as select distinct store from amazon_entry;
 
 drop view if exists vs_order;
-create view vs_order as select i.amazon_order_id, i.amazon_order_item_id, i.seller_sku, from_unixtime(o.data->'$.purchaseDate'/1000) as purchase_time, o.fulfillment, o.status from amazon_order o, amazon_order_item i where o.amazon_order_id = i.amazon_order_id;
-
+create view vs_order as select i.amazon_order_id, i.amazon_order_item_id, i.asin, i.seller_sku, from_unixtime(o.data->'$.purchaseDate'/1000) as purchase_time, o.fulfillment, o.status from amazon_order o, amazon_order_item i where o.amazon_order_id = i.amazon_order_id;
 
 drop view if exists vs_entry_order;
-create view vs_entry_order as select e.store, e.asin, e.sku, i.seller_sku, o.amazon_order_id, i.amazon_order_item_id, from_unixtime(o.data->'$.purchaseDate'/1000) as purchase_time, o.fulfillment, o.status from amazon_entry e, amazon_order o, amazon_order_item i where o.amazon_order_id = i.amazon_order_id and i.seller_sku like concat('%-', e.sku, '-%');
+create view vs_entry_order as select e.asin, e.sku, i.seller_sku, o.amazon_order_id, i.amazon_order_item_id, from_unixtime(o.data->'$.purchaseDate'/1000) as purchase_time, o.fulfillment, o.status from amazon_entry e, amazon_order o, amazon_order_item i where o.amazon_order_id = i.amazon_order_id and i.seller_sku like concat('%', e.sku, '-%');
+
+drop view if exists vs_entry_inventory;
+create view vs_entry_inventory as select e.asin, e.sku, i.seller_sku, i.in_stock_quantity, i.update_time from amazon_entry e, amazon_inventory i where i.seller_sku like concat('%', e.sku, '-%');
